ed3d7ccd74e932be1f4e4179a9f493b0a2cd0de6
Hopin-fing <dyggalag@gmail.com>
2021-07-29 14:30:10 +0300

    fix

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const config = require('config')
 const mongoose = require('mongoose')
+const path = require('path')
 const cors = require('cors');
 
 const app = express()
