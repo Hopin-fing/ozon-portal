4fa652df3d3084b45f64f45d291b5e6f59f4da8e
Hopin-fing <dyggalag@gmail.com>
2021-07-30 17:16:28 +0300

    fix

--- client/src/components/CommandPanel.js
+++ client/src/components/CommandPanel.js
@@ -55,7 +55,7 @@ const CommandPanel = () => {
     }
 
     const testRequestOnServer = async url => {
-        await fetch(url, {
+        const response = await fetch(url, {
             method: 'GET', // *GET, POST, PUT, DELETE, etc.
             mode: 'no-cors', // no-cors, *cors, same-origin
             cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
@@ -67,6 +67,7 @@ const CommandPanel = () => {
             redirect: 'follow', // manual, *follow, error
             referrerPolicy: 'no-referrer', // no-referrer, *client
         })
+        return await response.json()
     }
     useEffect(() => {
         if(allItems.length !== 0 ) {
