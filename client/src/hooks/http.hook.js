c00589f8a2965439b30b6a0cb688390092c1515d
Hopin-fing <dyggalag@gmail.com>
2021-07-30 16:38:14 +0300

    fix

--- client/src/hooks/http.hook.js
+++ client/src/hooks/http.hook.js
@@ -20,7 +20,7 @@ export const useHttp = () => {
             }
 
 
-           const response = await fetch( url, {method, body, headers})
+           const response = await fetch( url, {method, mode: 'no-cors', body, headers})
            const data = await response.json()
 
             if(!response.ok) {
