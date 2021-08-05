638fc2281c48b118c7b660bbe75708f5e9c8eb7b
Hopin-fing <dyggalag@gmail.com>
2021-08-05 12:27:07 +0300

    mod ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,14 @@
 module.exports = {
-  apps : [{
-    name   : "app1",
-    script : "./app.js"
-  }]
+  apps: [
+    {
+      name: "monit-price",
+      script: "./build/index",
+      watch: true,
+      instance_var: '1',
+      env: {
+        "PORT": 5000,
+        "NODE_ENV": "development"
+      }
+    }
+  ]
 }
