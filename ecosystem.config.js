79cafb6fc53795e9616634f8bd7ee5ec8af22eee
Hopin-fing <dyggalag@gmail.com>
2021-08-05 18:16:28 +0300

    fix ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,6 @@ module.exports = {
       script: "./build/index",
       watch: true,
       instance_var: '1',
-      max_memory_restart: '65M',
       env: {
         "PORT": 5000,
         "NODE_ENV": "development"
